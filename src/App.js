b2712fc0806d4fc719dc92fef732525c2451ae4d
thanx228 <dna_luxury@msn.com>
2021-09-16 10:48:33 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `you click ${count} times`;
+  }, [count]);
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <p>You Pick {count} time </p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <br />
+      <button onClick={() => setCount(0)}>Reset me</button>
     </div>
   );
 }
